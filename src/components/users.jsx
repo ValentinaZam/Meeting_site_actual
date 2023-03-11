0c17fe1d01c1a978a853bf9bd0c36df1c81452c4
Валентина <abc99-33@yandex.ru>
2023-03-11 13:22:26 +0300

    Доработка: условный  рендеринг

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -14,18 +14,18 @@ const Users = () => {
         setUsers(prevState => prevState.filter(tag => tag !== userId))
     };
 
-    const deleteTable = (number) => {
-        return (number === 0 ? "d-none": "table")
-    }
-
     const renderPhrase = (number) => {
         //Чтобы Сергей не ругался:) При увеличении количества users вставить функцию с большим количеством фильтров для number с проверкой на 22, 23 )
         return number === 0? "Никто с тобой не тусанёт" : (number<5 && number>1) ? `${number} человека тусанёт с тобой сегодня`: `${number} человек тусанёт с тобой сегодня`
     };
+
+    if(users.length === 0){
+        return <h2><span className={changeColorSpan(users.length)}>{renderPhrase(users.length)}</span></h2>
+    }
     return(
         <>
-        <span className={changeColorSpan(users.length)}>{renderPhrase(users.length)}</span>
-        <table className={deleteTable(users.length)}>
+        <h2><span className={changeColorSpan(users.length)}>{renderPhrase(users.length)}</span></h2>
+        <table className="table">
   <thead>
     <tr>
       <th scope="col">Имя</th>
