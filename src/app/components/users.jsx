c47b48b9ffb89448a82013405ef8c56b94c9b3cf
ValentinaZam <abc99-33@yandex.ru>
2023-04-10 22:06:12 +0300

    Фильтрация завершена

--- src/app/components/users.jsx
+++ src/app/components/users.jsx
@@ -1,51 +1,52 @@
-import React, { useState, useEffect } from "react";
-import { paginate } from "../utils/paginate";
-import Pagination from "./pagination";
-import User from "./user";
-import API from "../../api";
-import PropTypes from "prop-types";
-import GroupList from "./groupList";
-import SearchStatus from "./searchStatus";
+import React, { useState, useEffect } from "react"
+import { paginate } from "../utils/paginate"
+import Pagination from "./pagination"
+import User from "./user"
+import API from "../../api"
+import PropTypes from "prop-types"
+import GroupList from "./groupList"
+import SearchStatus from "./searchStatus"
 
 const Users = ({ users: allUsers, ...rest }) => {
-    const [currentPage, setCurrentPage] = useState(1);
-    const [professions, setProfessions] = useState();
-    const [selectorProf, setSelectorProf] = useState();
-
-    const pageSize = 4;
-
-    useEffect(() => {
-        API.professions.fetchAll().then((data) => setProfessions(data));
-    }, []);
+    const [currentPage, setCurrentPage] = useState(1)
+    const [professions, setProfessions] = useState()
+    const [selectеdProf, setSelectеdProf] = useState()
+    const pageSize = 4
 
     useEffect(() => {
-        setCurrentPage(1);
-    }, [selectorProf]);
+        API.professions.fetchAll().then((data) => setProfessions(data))
+    }, [])
+
+    useEffect(() => {
+        setCurrentPage(1)
+    }, [selectеdProf])
 
     const handleProfessionsSelect = item => {
-        setSelectorProf(item);
-    };
+        setSelectеdProf(item)
+    }
 
     const handlePageChange = (pageIndex) => {
-        setCurrentPage(pageIndex);
-    };
+        setCurrentPage(pageIndex)
+    }
 
-    const filteredUsers = selectorProf
-        ? allUsers.filter((user) => user.profession === selectorProf)
-        : allUsers;
-    const count = filteredUsers.length;
-    const userCrop = paginate(filteredUsers, currentPage, pageSize);
+    const filteredUsers = selectеdProf
+        ? allUsers.filter((user) => user.profession._id === selectеdProf._id)
+        : allUsers
+
+    const count = filteredUsers.length
+    const userCrop = paginate(filteredUsers, currentPage, pageSize)
 
     const clearFilter = () => {
-        setSelectorProf();
-    };
+        setSelectеdProf()
+    }
 
     return (
-
         <div className="d-flex">
+
             {professions && (
                 <div className="d-flex flex-column flex-shrink-0 p-3">
-                    <GroupList selectedItem={selectorProf} items={professions} onItemSelect={handleProfessionsSelect} />
+                    <GroupList selectedItem={selectеdProf} items={professions}
+                        onItemSelect={handleProfessionsSelect} />
                     <button className="btn btn-secondary mt-2" onClick={clearFilter}>Oчистить</button>
                 </div>
             )}
@@ -58,7 +59,7 @@ const Users = ({ users: allUsers, ...rest }) => {
                             <tr>
                                 <th scope="col">Имя</th>
                                 <th scope="col">Качества</th>
-                                <th scope="col">Провфессия</th>
+                                <th scope="col">Профессия</th>
                                 <th scope="col">Встретился, раз</th>
                                 <th scope="col">Оценка</th>
                                 <th scope="col">Избранное</th>
@@ -84,11 +85,11 @@ const Users = ({ users: allUsers, ...rest }) => {
             </div>
         </div>
 
-    );
-};
+    )
+}
 
 Users.propTypes = {
     users: PropTypes.array.isRequired
-};
+}
 
-export default Users;
+export default Users
