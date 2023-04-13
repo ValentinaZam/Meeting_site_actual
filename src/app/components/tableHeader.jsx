6cc1f922ba83739817d58f28af59b48b0b5d21bc
ValentinaZam <abc99-33@yandex.ru>
2023-04-13 20:16:22 +0300

    Сортировка завершена

--- src/app/components/tableHeader.jsx
+++ src/app/components/tableHeader.jsx
@@ -3,30 +3,31 @@ import PropTypes from "prop-types"
 
 const TableHeader = ({ onSort, selectedSort, columns }) => {
     const handlesSort = (item) => {
+        // console.log(item)
         if (selectedSort.path === item) {
             onSort({ ...selectedSort, order: selectedSort.order === "asc" ? "desc" : "asc" })
         } else {
             onSort({ path: item, order: "asc" })
         }
     }
+    const markingSort = (item) => {
+        if (selectedSort.path === undefined) {
+            return ""
+        } else if (selectedSort.path === item) {
+            return (<i className={"bi bi-caret-" + (selectedSort.order === "asc" ? "up-fill" : "down-fill")}></i>)
+        }
+    }
+
     return (<thead>
         <tr>
             {Object.keys(columns).map((column) => (
                 <th key={column}
                     onClick={columns[column].path ? () => handlesSort(columns[column].path) : undefined}
-                    scope="col" {...{ role: columns[column].path && "button" }}>{columns[column].name}
-                    {/* <i className="bi bi-caret-up-fill" onClick={columns[column].path ? () => handlesSort(columns[column].path) : undefined}></i> */}
+                    scope="col" {...{ role: columns[column].path && "button" }}>
+                    {columns[column].name}
+                    {markingSort(columns[column].path)}
                 </th>
             ))}
-            {/* <i class="bi bi-caret-down-fill"></i> - Вниз
-            <i class="bi bi-caret-up-fill"></i> - Вверх */}
-
-            {/* <th scope="col">Качества</th>
-            <th onClick={() => handlesSort("profession.name")} scope="col">Профессия</th>
-            <th onClick={() => handlesSort("completedMeetings")} scope="col">Встретился, раз</th>
-            <th onClick={() => handlesSort("rate")} scope="col">Оценка</th>
-            <th onClick={() => handlesSort("bookmark")} scope="col">Избранное</th>
-            <th /> */}
         </tr>
     </thead>)
 }
