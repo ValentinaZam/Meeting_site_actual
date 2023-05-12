d334555bfd7bad6345c8cecd5539a572086fb19d
ValentinaZam <abc99-33@yandex.ru>
2023-05-12 13:01:09 +0300

    Форма регистрации

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,8 +11,10 @@ module.exports = {
     },
     plugins: ["react"],
     rules: {
+        // semi: [2, "always"],
         indent: [0, 4],
         quotes: ["error", "double", { allowTemplateLiterals: true }],
-        "space-before-function-paren": ["error", { anonymous: "always", named: "never" }]
+        "space-before-function-paren": ["error", { anonymous: "always", named: "never" }],
+        "multiline-ternary": ["off"]
     }
 }
