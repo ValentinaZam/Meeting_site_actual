41a87156385e319f4637499704514882ff51550b
ValentinaZam <abc99-33@yandex.ru>
2023-05-19 13:39:08 +0300

    Подготовка к теме расширенные хуки и базовая оптимизация

--- src/app/components/common/form/selectField.jsx
+++ src/app/components/common/form/selectField.jsx
@@ -9,22 +9,32 @@ const SelectField = ({ label, value, onChange, defaultOption, options, error, na
         return "form-select" + (error ? " is-invalid" : "")
     }
     const optionsArray = !Array.isArray(options) && typeof options === "object"
-        ? Object.keys(options).map((optionName) => ({ name: options[optionName].name, value: options[optionName]._id })) : options
+        ? Object.values(options)
+        : options
 
     return (
         <div className="mb-4">
             <label htmlFor={name} className="form-label">
                 {label}
             </label>
-            <select className={getInputClasses()} id={name} name={name} value={value} onChange={handleChange}>
-                <option disabled value="">{defaultOption}</option>
-                {optionsArray && optionsArray.map(option => <option key={option._id}
-                    value={option.value}
-                >{option.name}</option>)}
+            <select
+                className={getInputClasses()}
+                id={name}
+                name={name}
+                value={value}
+                onChange={handleChange}
+            >
+                <option disabled value="">
+                    {defaultOption}
+                </option>
+                {optionsArray.length > 0 &&
+                    optionsArray.map((option) => (
+                        <option value={option.value} key={option.value}>
+                            {option.label}
+                        </option>
+                    ))}
             </select>
-            {error && <div className="invalid-feedback">
-                {error}
-            </div>}
+            {error && <div className="invalid-feedback">{error}</div>}
         </div>
     )
 }
